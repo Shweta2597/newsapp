9850c003cfc1d228cb81d7b9f1f5ca22149babcc
shweta255178 <shwetamds333@gmail.com>
2023-09-14 03:25:00 +0530

    Using fetch api to populate data

--- src/components/News.js
+++ src/components/News.js
@@ -2,14 +2,37 @@ import React, { Component } from 'react'
 import NewsItem from './NewsItem'
 
 export class News extends Component {
+    articles=[]
+    constructor(){
+        super();
+        console.log("Constructur here from news component");
+        this.state = {
+            articles: this.articles,
+            loading: false,
+        }
+    }
+
+    async componentDidMount(){
+        let url = "https://newsapi.org/v2/top-headlines?country=in&apiKey=3c838f43c72d4899a199e23511e0824c";
+        let data = await fetch(url)
+        let parseData = await data.json();
+        this.setState({
+            articles: parseData.articles 
+        })
+    }
+
   render() {
     return (
-      <div>
-        News Component
-        <NewsItem></NewsItem>
-        <NewsItem></NewsItem>
-        <NewsItem></NewsItem>
-        <NewsItem></NewsItem>
+      <div className='container my-3'> 
+        <h2>News Monkey - Top Headlines</h2>
+        <div className="row">
+        {this.state.articles.map((element)=>{
+            return <div className="col-md-4" key={element.url}>
+            <NewsItem  title={element.title?element.title.slice(0,45):'No title'} description={element.description?element.description.slice(0,88):"No Description"} imageUrl={element.urlToImage} newsUrl={element.url}></NewsItem>
+        </div>
+        })}
+            
+        </div>
       </div>
     )
   }
--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -1,11 +1,21 @@
 import React, { Component } from 'react'
 
 export class NewsItem extends Component {
+
+
   render() {
+    let {title,description,imageUrl,newsUrl} = this.props
     return (
-      <div>
-        This is a news item 2
-      </div>
+            <div className='my-3'>
+                <div className="card" style={{width : '18rem'}}>
+                <img src={imageUrl} style={{width:'fit width' , height:"12rem"}} className="card-img-top" alt="..."/>
+                <div className="card-body">
+                    <h5 className="card-title" data-bs-toggle="tooltip" style={{height:"4rem"}}>{title}...</h5>
+                    <p className="card-text" style={{height:"6rem"}}>{description}...</p>
+                    <a href={newsUrl} target="_blank" className="btn btn-sm btn-primary">Read More</a>
+                </div>
+                </div>
+            </div>
     )
   }
 }
